ec473221a813af3ccebfadcaec8731fdfad1420b
rioalsafe <36718599+rioalsafe@users.noreply.github.com>
2021-12-02 10:15:45 +0900

    Update Alpha.O.02Dlg.cpp

--- Alpha.O.02Dlg.cpp
+++ Alpha.O.02Dlg.cpp
@@ -1,4 +1,4 @@
-// Alpha.O.02Dlg.cpp: ���� ����
+// Alpha.O.02Dlg.cpp: 구현 파일
 //
 #include <stdio.h>
 #include "pch.h"
@@ -14,7 +14,7 @@
 
 
 
-// CAlphaO02Dlg ��ȭ ����
+// CAlphaO02Dlg 대화 상자
 
 
 CAlphaO02Dlg::CAlphaO02Dlg(CWnd* pParent /*=nullptr*/)
@@ -46,28 +46,28 @@ BEGIN_MESSAGE_MAP(CAlphaO02Dlg, CDialogEx)
 END_MESSAGE_MAP()
 
 
-// CAlphaO02Dlg �޽��� ó����
+// CAlphaO02Dlg 메시지 처리기
 
 BOOL CAlphaO02Dlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
-	// �� ��ȭ ������ �������� �����մϴ�.  ���� ���α׷��� �� â�� ��ȭ ���ڰ� �ƴ� ��쿡��
-	//  �����ӿ�ũ�� �� �۾��� �ڵ����� �����մϴ�.
-	SetIcon(m_hIcon, TRUE);			// ū �������� �����մϴ�.
-	SetIcon(m_hIcon, FALSE);		// ���� �������� �����մϴ�.
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
-	// TODO: ���⿡ �߰� �ʱ�ȭ �۾��� �߰��մϴ�
+	// TODO: 여기에 추가 초기화 작업을 추가합니다
 
-	return TRUE;  // ��Ŀ���� ��Ʈ�ѿ� �������� ������ TRUE�� ��ȯ�մϴ�.
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
 
-// ��ȭ ���ڿ� �ּ�ȭ ���߸� �߰��� ��� �������� �׸�����
-//  �Ʒ� �ڵ尡 �ʿ��մϴ�.  ����/�� ���� ����ϴ� MFC ���ø����̼��� ��쿡��
-//  �����ӿ�ũ���� �� �۾��� �ڵ����� �����մϴ�.
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
 
-// ����ڰ� �ּ�ȭ�� â�� ���� ���ȿ� Ŀ���� ǥ�õǵ��� �ý��ۿ���
-//  �� �Լ��� ȣ���մϴ�.
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
 HCURSOR CAlphaO02Dlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
@@ -77,17 +77,17 @@ HCURSOR CAlphaO02Dlg::OnQueryDragIcon()
 
 void CAlphaO02Dlg::OnPaint()
 {
-	CPaintDC dc(this); // �׸��⸦ ���� ����̽� ���ؽ�Ʈ�Դϴ�.
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
 
-	if (IsIconic()) {} // �� ���ø����̼��� �ּ�ȭ �Ǿ��� �� �̷��� ó���ϰڴٴ� ���ǹ�. ������ ��� X. ����! 	} 
+	if (IsIconic()) {} // 내 어플리케이션이 최소화 되었을 때 이렇게 처리하겠다는 조건문. 지금은 사용 X. 생략! 	} 
 	else
 	{
 
-		CBrush MyBrush(RGB(248, 207, 143));	 //�ٵ��� ��ä���
+		CBrush MyBrush(RGB(248, 207, 143));	 //바둑판 색채우기
 		dc.SelectObject(&MyBrush);
 
 
-		for (int x = 0; x < 18; x++) //19x19 �ٵ��� �׸���
+		for (int x = 0; x < 18; x++) //19x19 바둑판 그리기
 		{
 			for (int y = 0; y < 18; y++)
 			{
@@ -100,14 +100,14 @@ void CAlphaO02Dlg::OnPaint()
 			}
 		}
 
-		dc.SelectStockObject(DC_BRUSH);		//�ٵ��Ǿȿ� �ٵϵ� �׸���
+		dc.SelectStockObject(DC_BRUSH);		//바둑판안에 바둑돌 그리기
 		for (int x = 0; x < 19; x++)
 		{
 			for (int y = 0; y < 19; y++)
 			{
-				if (dol[y][x]) // 0�̸� �׸��� �ʴ´�.
+				if (dol[y][x]) // 0이면 그리지 않는다.
 				{
-					dc.SetDCBrushColor(0x00FFFFFF * (dol[y][x] - 1)); // +1�� ���� ���������� �ٽ� -1�� �Ͽ� step���� �����Ѵ�.
+					dc.SetDCBrushColor(0x00FFFFFF * (dol[y][x] - 1)); // +1한 값을 저장했으니 다시 -1을 하여 step값을 복구한다.
 					dc.Ellipse(x * WIDTH + OFFSET - (WIDTH / 2),
 						y * WIDTH + OFFSET - (WIDTH / 2),
 						x * WIDTH + WIDTH + OFFSET - (WIDTH / 2),
@@ -121,7 +121,7 @@ void CAlphaO02Dlg::OnPaint()
 
 
 
-//��Ŭ���� ������ �δ� �̺�Ʈ
+//좌클릭시 오목돌을 두는 이벤트
 void CAlphaO02Dlg::OnLButtonDown(UINT nFlags, CPoint point)
 {
 	int index_x;
@@ -133,7 +133,7 @@ void CAlphaO02Dlg::OnLButtonDown(UINT nFlags, CPoint point)
 	if (pause_count == 0) {
 		if (index_x < 19 && index_y < 19 && index_x >= 0 && index_y >= 0 && dol[index_y][index_x] == 0)
 		{
-			// Ÿ�̸� �ʱ�ȭ, Ÿ�̸Ӹ���Ʈ �ʱ�ȭ, ���� �ð� �Է�
+			// 타이머 초기화, 타이머리스트 초기화, 제한 시간 입력
 			KillTimer(1);
 			m_timer.ResetContent();
 			turn = TURNTIME;
@@ -141,7 +141,7 @@ void CAlphaO02Dlg::OnLButtonDown(UINT nFlags, CPoint point)
 
 			if (index_x < 19 && index_y < 19 && index_x >= 0 && index_y >= 0)
 			{
-				dol[index_y][index_x] = step + 1; // �������̸� 0 + 1 , ���̸� 1+1 �� ���Ե�.
+				dol[index_y][index_x] = step + 1; // 검은돌이면 0 + 1 , 흰돌이면 1+1 이 들어가게됨.
 				InvalidateRect(CRect(index_x * WIDTH - (WIDTH / 2) + OFFSET,
 					index_y * WIDTH - (WIDTH / 2) + OFFSET,
 					index_x * WIDTH - (WIDTH / 2) + OFFSET + WIDTH,
@@ -152,10 +152,10 @@ void CAlphaO02Dlg::OnLButtonDown(UINT nFlags, CPoint point)
 			}
 		}
 	}
-	else AfxMessageBox(_T("������ư�� �������ּ���"));
+	else AfxMessageBox(_T("정지버튼을 해제해주세요"));
 
 
-	if (dol[index_y][index_x] != 0) // ���� �¸� �Ǻ�
+	if (dol[index_y][index_x] != 0) // 오목 승리 판별
 	{
 		for (int i = 0; i < 5; i++)
 		{
@@ -211,7 +211,7 @@ void CAlphaO02Dlg::OnLButtonDown(UINT nFlags, CPoint point)
 }
 
 
-//��Ŭ���� ������ Ÿ�̸� �۵�
+//좌클릭을 땠을때 타이머 작동
 void CAlphaO02Dlg::OnLButtonUp(UINT nFlags, CPoint point)
 {
 	int index_x;
@@ -228,7 +228,7 @@ void CAlphaO02Dlg::OnLButtonUp(UINT nFlags, CPoint point)
 }
 
 
-//���� ��ư Ŭ����
+//정지 버튼 클릭시
 void CAlphaO02Dlg::OnBnClickedPause()
 {
 	KillTimer(1);
@@ -239,7 +239,7 @@ void CAlphaO02Dlg::OnBnClickedPause()
 }
 
 
-//���� ��ư Ŭ���� �̺�Ʈ
+//저장 버튼 클릭시 이벤트
 void CAlphaO02Dlg::OnBnClickedSave()
 {
 	OnSave();
@@ -247,7 +247,7 @@ void CAlphaO02Dlg::OnBnClickedSave()
 	//m_dol_log.ResetContent();
 }
 
-//�α� �о���� ��ư Ŭ���� �̺�Ʈ
+//로그 읽어오기 버튼 클릭시 이벤트
 void CAlphaO02Dlg::OnBnClickedLoad()
 {
 	m_dol_log.ResetContent();
@@ -256,7 +256,7 @@ void CAlphaO02Dlg::OnBnClickedLoad()
 
 
 
-//�¸� �޼��� ����
+//승리 메세지 전달
 void CAlphaO02Dlg::WhoIsWinner(int sum)
 {
 	CString strMessage;
@@ -264,23 +264,23 @@ void CAlphaO02Dlg::WhoIsWinner(int sum)
 
 
 
-	KillTimer(1);	//�̰����� Ÿ�̸� ����
+	KillTimer(1);	//이겼을시 타이머 종료
 
 	if (sum == 10) {
-		//strMessage = L"�����մϴ� �鵹���� �¸� �ϼ̽��ϴ�\r\n";
+		//strMessage = L"축하합니다 백돌님이 승리 하셨습니다\r\n";
 		white_win_count++;
 		m_dol_log.InsertString(-1, wWin);
 	}
 
 	else if (sum == 5) {
-		//strMessage = L"�����մϴ� �浹���� �¸� �ϼ̽��ϴ�\r\n";
+		//strMessage = L"축하합니다 흑돌님이 승리 하셨습니다\r\n";
 		black_win_count++;
 		m_dol_log.InsertString(-1, bWin);
 	}
 	else
 		return;
 
-	//�������� ȣ��
+	//최종승자 호출
 	WhoIsLastWinner();
 
 	CClientDC dc(this);
@@ -288,11 +288,11 @@ void CAlphaO02Dlg::WhoIsWinner(int sum)
 	int left = 20 * WIDTH - (WIDTH / 2) + OFFSET;
 	dc.DrawText(strMessage, CRect(left, 100, left + 300, 200), DT_SINGLELINE | DT_LEFT);
 
-	strBoxMessage = strMessage + L"�� ���� �� �Ͻðڽ��ϱ�?";
+	strBoxMessage = strMessage + L"한 게임 더 하시겠습니까?";
 
 	if (IDOK == AfxMessageBox(strBoxMessage, MB_OKCANCEL))
 	{
-		// �¸� ������ ������ ȭ���� ��� ��������, ���� ���� ����ִ� �迭�� �ʱ�ȭ�Ͽ� ���� ������ ������ �� �ֵ��� �ߴ�. 
+		// 승리 판정이 끝나면 화면이 모두 지워지고, 돌의 값이 들어있는 배열도 초기화하여 다음 게임을 진행할 수 있도록 했다. 
 		memset(dol, 0, sizeof(int) * 19 * 19);
 		Invalidate();
 		step = false;
@@ -303,7 +303,7 @@ void CAlphaO02Dlg::WhoIsWinner(int sum)
 	}
 }
 
-//�������� ����
+//최종승자 검정
 void CAlphaO02Dlg::WhoIsLastWinner()
 {
 	m_winner.ResetContent();
@@ -321,7 +321,7 @@ void CAlphaO02Dlg::WhoIsLastWinner()
 
 
 
-//�¸� �α� ����
+//승리 로그 저장
 void CAlphaO02Dlg::OnSave(void)
 {
 
@@ -329,7 +329,7 @@ void CAlphaO02Dlg::OnSave(void)
 
 	CString m_strPath, str;
 	FILE* m_pFile;
-	TCHAR BASED_CODE szFilter[] = _T("�ؽ�Ʈ ����(*.TXT) | *.TXT;*.txt; | �������(*.*) |*.*|");
+	TCHAR BASED_CODE szFilter[] = _T("텍스트 파일(*.TXT) | *.TXT;*.txt; | 모든파일(*.*) |*.*|");
 	CFileDialog dlg(FALSE, _T("*.txt"), 0, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, szFilter);
 
 	if (dlg.DoModal() == IDOK) {
@@ -339,7 +339,7 @@ void CAlphaO02Dlg::OnSave(void)
 		if (err != 0) { return; }
 		CStdioFile m_File(m_pFile);
 
-		//����Ʈ�ڽ��� �ؽ�Ʈ�� ���پ� txt���Ϸ� �ۼ�
+		//리스트박스의 텍스트를 한줄씩 txt파일로 작성
 		for (int i = 0; i < m_dol_log.GetCount(); ++i) {
 			m_dol_log.GetText(i, m_strPath);
 			m_strPath += L"\n";
@@ -352,7 +352,7 @@ void CAlphaO02Dlg::OnSave(void)
 
 }
 
-//�¸� �α� �ҷ�����
+//승리 로그 불러오기
 void CAlphaO02Dlg::OnLoad(void)
 {
 	setlocale(LC_ALL, "");
@@ -360,16 +360,16 @@ void CAlphaO02Dlg::OnLoad(void)
 	m_timer.ResetContent();
 	m_winner.ResetContent();
 	KillTimer(1);
-	//�� �¸�ī��Ʈ, �� �¸�ī��Ʈ �ʱ�ȭ
+	//흑 승리카운트, 백 승리카운트 초기화
 	black_win_count = 0;
 	white_win_count = 0;
 
 	CString display_str, strPath, win_log;
 	FILE* m_pFile;
-	TCHAR BASED_CODE szFilter[] = _T("�ؽ�Ʈ ����(*.TXT) | *.TXT;*.txt; | �������(*.*) |*.*|");
+	TCHAR BASED_CODE szFilter[] = _T("텍스트 파일(*.TXT) | *.TXT;*.txt; | 모든파일(*.*) |*.*|");
 	CFileDialog dlg(FALSE, _T("*.txt"), 0, OFN_HIDEREADONLY, szFilter);
 
-	//txt���Ͽ��� �α� �о����
+	//txt파일에서 로그 읽어오기
 	if (dlg.DoModal() == IDOK) {
 		strPath = dlg.GetPathName();
 
@@ -387,19 +387,20 @@ void CAlphaO02Dlg::OnLoad(void)
 
 	}
 
-	// m_dol_log�� �ִ� �ε������� ����Ʈ������ dol_log_copy�� �ű��
 
 
+	//백승리수 흑승리수 찾기
 
-	// CString���� ���� win_log ���� ��¸��� ��¸��� ã��
-	white_win_count = GetFindCharCount(GetTextListBox(), 'H');
-	black_win_count = GetFindCharCount(GetTextListBox(), 'B');
+	CString string_dol_log = GetTextListBox();
+
+	white_win_count = GetFindCharCount(string_dol_log, 'H');
+	black_win_count = GetFindCharCount(string_dol_log, 'B');
 
 	WhoIsLastWinner();
 
 }
 
-//����Ʈ�ڽ��� �ִ� ���ڿ��� CString���� �о����
+//리스트박스에 있는 문자열을 CString으로 읽어오기
 CString CAlphaO02Dlg::GetTextListBox() {
 
 	CString load_dol_log, dol_log_copy;
@@ -416,7 +417,7 @@ CString CAlphaO02Dlg::GetTextListBox() {
 
 
 
-//CString -> �迭
+//CString -> 배열
 int CAlphaO02Dlg::GetFindCharCount(CString parm_string, char parm_find_char) {
 	int length = parm_string.GetLength(), find_count = 0;
 
@@ -431,16 +432,16 @@ int CAlphaO02Dlg::GetFindCharCount(CString parm_string, char parm_find_char) {
 }
 
 
-// Ÿ�̸� �̺�Ʈ
+// 타이머 이벤트
 void CAlphaO02Dlg::OnTimer(UINT_PTR nIDEvent)
 {
-	m_timer.ResetContent();	//Ÿ�̸Ӹ���Ʈ�ڽ� �ʱ�ȭ
+	m_timer.ResetContent();	//타이머리스트박스 초기화
 	CString timenumber;
 	CString wWin;
 
 	switch (nIDEvent)
 	{
-	case 1: // Ÿ�̸�(1)
+	case 1: // 타이머(1)
 		timenumber.Format(_T(" %d"), turn);
 		m_timer.InsertString(0, timenumber);
 		turn--;
@@ -448,11 +449,11 @@ void CAlphaO02Dlg::OnTimer(UINT_PTR nIDEvent)
 	}
 
 
-	if (turn == -1) {	//Ÿ�̸ӽð��� 0�� �ɽ� ���� �� ����
+	if (turn == -1) {	//타이머시간이 0이 될시 현재 턴 종료
 		KillTimer(1);
-		//CString wWin = _T("���� �Ѿ�ϴ�");
+		//CString wWin = _T("턴이 넘어갑니다");
 		//AfxMessageBox(wWin);
-		wWin = L"���� �Ѿ�ϴ�";
+		wWin = L"턴이 넘어갑니다";
 		if (IDOK == AfxMessageBox(wWin)) SetTimer(1, 1000, NULL);
 
 		m_timer.ResetContent();
@@ -465,7 +466,7 @@ void CAlphaO02Dlg::OnTimer(UINT_PTR nIDEvent)
 
 
 
-//���� ��ư
+//리셋 버튼
 void CAlphaO02Dlg::OnBnClickedReset()
 {
 	KillTimer(1);
@@ -515,19 +516,19 @@ void CAlphaO02Dlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
 	if (nID == SC_CLOSE)
 	{
-		//�����ư ���� ��
+		//종료버튼 눌릴 시
 	}
 	else if (nID == SC_MAXIMIZE)
 	{
-		//�ִ�ȭ ��ư ���� ��
+		//최대화 버튼 눌릴 시
 	}
 	else if (nID == SC_MINIMIZE)
 	{
-		//�ּ�ȭ ��ư ���� ��
+		//최소화 버튼 눌릴 시
 	}
 	else if (nID == SC_RESTORE)
 	{
-		//���� ��Ȳ����
+		//복원 상황에서
 	}
 	CDialogEx::OnSysCommand(nID, lParam);
 }
